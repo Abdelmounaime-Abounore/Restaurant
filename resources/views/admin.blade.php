09ad9533c57dc3b2853eea09a846fd937a2c4747
Abdelmounaim-Abounore <abdelmounaim.abounore@gmail.com>
2023-02-04 19:31:50 +0100

    authentication created

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,11 @@
+@include('layouts.master')
+        <article class="my-4">
+            <h2>Plat 1</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate minus placeat neque magni nemo! Numquam culpa praesentium sed, est sit provident illo odio reprehenderit, ipsum aut corrupti nisi repellendus quibus</p>
+        </article>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+
+    </body>
+</html>
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Master</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="/css/app.css" />
+        <script src="{{ asset('js/app.js') }}" defer></script>
+        
+    </head>
+    <body >
+        <nav class="navbar navbar-expand-lg navbar-light bg-light px-3 w-100">
+            {{-- <img src="./images/logo.png" alt="logo"> --}}
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse mx-5" id="navbarNav">
+                <ul class="navbar-nav mx-5 w-75 d-flex justify-content-between">
+                    <li class="nav-item active mx-4">
+                        <a class="nav-link" href="./dashboard.php">Dashboard</a>
+                    </li>
+                    <li class="nav-item mx-4">
+                        <a class="nav-link" href="./home.php">Home</a>
+                    </li>
+                    <li class="nav-item mx-4">
+                        <a class="nav-link" href="./add.article.php">Add Articles</a>
+                    </li>
+                    <li class="nav-item mx-4 dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }}
+                        </a>
+
+                        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                               onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
+                                {{ __('Logout') }}
+                            </a>
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </nav>
